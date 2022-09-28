625bd9a50c3f3b7d9acff5cc27e1938fca592bdc
dev-newton <newtonimonjirie@gmail.com>
2022-09-28 09:07:12 +0100

    refactor: clean up sidebar

---
+++ src/components/Dashboard/Sidebar/SidebarItems.ts
@@ -0,0 +1,55 @@
+import homeIcon from "assets/home.svg";
+import loanImg from "assets/loans.svg";
+import calcImg from "assets/calcImg.svg";
+import liquidateImg from "assets/liquidate.svg";
+import supportImg from "assets/support.svg";
+import settingstImg from "assets/settings-avatar.svg";
+import accountImg from "assets/account-avatar.svg";
+import historyImg from "assets/history-icon.svg";
+
+const SidebarItems =[
+    {
+        title:"home",
+        path:"/home",
+        icon: homeIcon
+    },
+    {
+        title:"my loans",
+        path:"/loans",
+        icon: loanImg
+    },
+    {
+        title:"loan calculator",
+        path:"/loan-calculator",
+        icon: calcImg
+    },
+    {
+        title:"liquidate",
+        path:"/liquidate",
+        icon: liquidateImg
+    },
+    {
+        title:"support",
+        path:"/support",
+        icon: supportImg
+    },
+    {
+        title:"settings",
+        path:"/settings",
+        icon: settingstImg
+    },
+
+    {
+        title:"account",
+        path:"/account",
+        icon: accountImg
+    },
+    {
+        title:"history",
+        path:"/history",
+        icon: historyImg
+    },
+]
+
+ 
+export default SidebarItems
