24f65caa2fae930b1f527286e2d4c82eb542a22a
dev-newton <newtonimonjirie@gmail.com>
2022-09-29 13:35:04 +0100

    refactor: make cards take up entire width of screen

--- src/components/Dashboard/Home/account-summary.tsx
+++ src/components/Dashboard/Home/account-summary.tsx
@@ -1,18 +1,17 @@
-import AccountSum from "./account-sum"
-import "./index.css"
+import AccountSum from "./account-sum";
+import "./index.css";
 
 const Summary = () => {
-    return (
-        <div className="summary">
-            {AccountSum.map((item) => (
-               <div className={`summary-box ${item.classname}`}>
-                    <div className="title">{item.title}</div>
-                    <div className="value">{item.value}</div>
-               </div>
-            ))}
+  return (
+    <div className="summary">
+      {AccountSum.map((item) => (
+        <div className={`summary-box ${item.classname}`}>
+          <div className="title">{item.title}</div>
+          <div className="value">{item.value}</div>
         </div>
-
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Summary;
--- src/pages/Dashboard/Home/index.tsx
+++ src/pages/Dashboard/Home/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import Greeting from "../../../components/Dashboard/Home/Greeting"
-import Summary from "../../../components/Dashboard/Home/account-summary"
+import Greeting from "../../../components/Dashboard/Home/Greeting";
+import Summary from "../../../components/Dashboard/Home/account-summary";
 
 const Home = () => {
   return (
     <div>
-      <Greeting/>
-      <Summary/>
+      <Greeting />
+      <Summary />
+      <div className="products-section">
+        <h2>My Products</h2>
+        <div className="products-wrapper">
+          <div className="product"></div>
+        </div>
+      </div>
     </div>
   );
 };
