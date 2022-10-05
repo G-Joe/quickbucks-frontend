8e4daf8e000fc15d1808f187b773b7182794dcaf
dev-newton <newtonimonjirie@gmail.com>
2022-10-05 09:42:51 +0100

    added modal

--- src/pages/Dashboard/Loans/components/LoanTab/LoanTab.tsx
+++ src/pages/Dashboard/Loans/components/LoanTab/LoanTab.tsx
@@ -1,10 +1,19 @@
 import { useState } from "react";
+import { Modal } from "antd";
+
 import Benefit from "../Benefit/Benefit";
 import { Button, Input } from "components";
+import cancelIcon from "assets/cancel.svg";
+import checkIcon from "assets/checked-success.svg";
 import "./LoanTab.scss";
+import useDisclosure from "components/shared/Modal/useDisclosure";
 
 const LoanTab = ({ title, amount, tenor, taken, outstanding }: any) => {
   const [checked, setChecked] = useState(false);
+
+  const modalPin = useDisclosure();
+  const modalSuccess = useDisclosure();
+  const modalPay = useDisclosure();
 
   return (
     <>
@@ -90,6 +99,7 @@ const LoanTab = ({ title, amount, tenor, taken, outstanding }: any) => {
                 <Button
                   label="continue"
                   variant={checked ? "primary" : "disabled"}
+                  onClick={modalPin.onOpen}
                 />
               </div>
             </div>
@@ -106,11 +116,92 @@ const LoanTab = ({ title, amount, tenor, taken, outstanding }: any) => {
                 <p>Loan outstanding </p>
                 <h3>{outstanding}</h3>
               </div>
-              <Button label="Pay Back" variant="primary" />
+              <Button
+                onClick={modalPay.onOpen}
+                label="Pay Back"
+                variant="primary"
+              />
             </div>
           </div>
         </div>
       </div>
+      <Modal
+        open={modalPin.isOpen}
+        onOk={modalPin.onClose}
+        className="pin-modal"
+        centered
+      >
+        <div className="modal-container">
+          <h3>Please enter your PIN </h3>
+          <Input label="PIN" type="password" secure />
+          <Button
+            label="Submit"
+            variant="primary"
+            onClick={() => {
+              modalPin.onClose();
+              modalSuccess.onOpen();
+            }}
+          />
+          <img
+            className="close-icon"
+            onClick={modalPin.onClose}
+            src={cancelIcon}
+            alt="cancelIcon"
+          />
+        </div>
+      </Modal>
+      <Modal
+        open={modalSuccess.isOpen}
+        onOk={modalSuccess.onClose}
+        className="success-modal"
+        centered
+      >
+        <div className="modal-container">
+          <img className="check-icon" src={checkIcon} alt="checkIcon" />
+          <h3>Loan Request Successful </h3>
+          <Button
+            label="Go Home"
+            variant="primary"
+            onClick={() => {
+              modalSuccess.onClose();
+            }}
+          />
+          <img
+            className="close-icon"
+            onClick={modalSuccess.onClose}
+            src={cancelIcon}
+            alt="cancelIcon"
+          />
+        </div>
+      </Modal>
+      <Modal
+        open={modalPay.isOpen}
+        onOk={modalPay.onClose}
+        className="payback-modal"
+        centered
+      >
+        <div className="modal-container">
+          <div className="outstanding-box">
+            <p>Loan amount outstanding</p>
+            <b>N239,571.00</b>
+          </div>
+          <Input label="Enter Amount{N}" type="text" />
+          <Button
+            label="Liquidate"
+            variant="primary"
+            onClick={() => {
+              modalPay.onClose();
+              modalPin.onOpen();
+            }}
+          />
+          <img
+            className="close-icon"
+            onClick={modalPay.onClose}
+            src={cancelIcon}
+            alt="cancelIcon"
+          />
+        </div>
+      </Modal>
     </>
   );
 };
