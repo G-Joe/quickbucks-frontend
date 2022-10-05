5e3f2808ee0226fabaf3406a59eb4c98f5328761
dev-newton <newtonimonjirie@gmail.com>
2022-10-05 03:56:37 +0100

    fix major styling issue

--- src/pages/Dashboard/Loans/index.tsx
+++ src/pages/Dashboard/Loans/index.tsx
@@ -3,43 +3,45 @@ import { Input } from "components";
 
 const Loans = () => {
   return (
-    <div>
-      <div className="header">
-        <div className="header-logo-wrapper">
-          <img src={headerLogo} alt="headerLogo" />
+    <>
+      <div className="main-fit">
+        <div className="header">
+          <div className="header-logo-wrapper">
+            <img src={headerLogo} alt="headerLogo" />
+          </div>
+          <h3>My Loans</h3>
         </div>
-        <h3>My Loans</h3>
-      </div>
 
-      <div className="loan-card">
-        <div className="loan-card-contain">
-          <div className="eligible-card">
-            <div className="eligible-card-content">
-              <p>Eligible Amount</p>
-              <h4>N92,000.00</h4>
+        {/* <div className="loan-card">
+          <div className="loan-card-contain">
+            <div className="eligible-card">
+              <div className="eligible-card-content">
+                <p>Eligible Amount</p>
+                <h4>N92,000.00</h4>
+              </div>
+              <div className="eligible-card-content">
+                <p>Tenor</p>
+                <h4>N92,000.00</h4>
+              </div>
             </div>
-            <div className="eligible-card-content">
-              <p>Tenor</p>
-              <h4>N92,000.00</h4>
+
+            <p>Enter amount less than N92,000.00</p>
+
+            <Input label="Amount" type="text" />
+
+            <div className="eligible-card">
+              <div className="content">
+                <p>
+                  Repayment Amount: <br />
+                  (Amount you will need to repay on due)
+                </p>
+                <p>N105,150</p>
+              </div>
             </div>
           </div>
-
-          <p>Enter amount less than N92,000.00</p>
-
-          <Input label="Amount" type="text" />
-
-          <div className="eligible-card">
-            <div className="content">
-              <p>
-                Repayment Amount: <br />
-                (Amount you will need to repay on due)
-              </p>
-              <p>N105,150</p>
-            </div>
-          </div>
-        </div>
+        </div> */}
       </div>
-    </div>
+    </>
   );
 };
 
