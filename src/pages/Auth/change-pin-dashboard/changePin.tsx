2f5f71e763afab1d4f5ce4330ff8524da553f258
dev-newton <newtonimonjirie@gmail.com>
2022-10-14 06:41:09 +0100

    refactored settings page

--- src/pages/Auth/change-pin-dashboard/changePin.tsx
+++ src/pages/Auth/change-pin-dashboard/changePin.tsx
@@ -1,27 +1,21 @@
 import Input from "components/shared/Input/Input";
-import Button from "components/shared/Button/Button"
-import SettingsImg from "assets/settings-dashboard.svg"
-import "styles/index.scss"
+import Button from "components/shared/Button/Button";
+import SettingsImg from "assets/settings-dashboard.svg";
+import "styles/index.scss";
 
 const ChangePin = () => {
   return (
-<div className="Settings">
-  <div className="DashboardTopBar">
-        <img className="settingsImg" src={SettingsImg} alt="settingImg"/>
-        <p className="settingTxt">Settings</p> 
-  </div>
-  <div className="form-wrapper">
-    <form className="form">
-        <Input label="Old PIN" type="password" secure />
-        <Input label="New PIN" type="password" secure />
-        <Input label="Confirm PIN" type="password" secure />
-        <Button to="#" label="submit" variant="primary" />
-    </form>
-  </div>
-</div>  
+    <div className="Settings">
+      <div className="form-wrapper">
+        <form className="form">
+          <Input label="Old PIN" type="password" secure />
+          <Input label="New PIN" type="password" secure />
+          <Input label="Confirm PIN" type="password" secure />
+          <Button to="#" label="submit" variant="primary" />
+        </form>
+      </div>
+    </div>
+  );
+};
 
-
-  )
-}
-
-export default ChangePin
+export default ChangePin;
