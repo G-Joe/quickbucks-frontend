94c99cb960e88d3f2f91a87697c448d239091d6e
unogjoe <nketageorgejoe@gmail.com>
2022-10-18 13:17:38 +0100

    added sub pages to Account

--- src/pages/Dashboard/Account/index.tsx
+++ src/pages/Dashboard/Account/index.tsx
@@ -1,16 +1,16 @@
-import React from "react";
 import accountLogo from "assets/account.svg"
 import Header from "components/Dashboard/Header/Header";
 
 const Account = () => {
   return (
     <div>
-       <div className="main-fit">
+      <div className="main-fit">
         <Header img={accountLogo} title="Account" />
       </div>
-      
     </div>
   );
 };
+
+
 
 export default Account;
