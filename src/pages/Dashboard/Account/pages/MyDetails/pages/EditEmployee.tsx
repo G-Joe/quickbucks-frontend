86c56b4eeaa44d844bf26946f9acfcfa9247af94
dev-newton <newtonimonjirie@gmail.com>
2022-10-27 08:46:31 +0100

    add modal to my details

--- src/pages/Dashboard/Account/pages/MyDetails/pages/EditEmployee.tsx
+++ src/pages/Dashboard/Account/pages/MyDetails/pages/EditEmployee.tsx
@@ -1,6 +1,13 @@
+import { Modal } from "antd";
 import { Button, Input } from "components";
+import useDisclosure from "components/shared/Modal/useDisclosure";
+import cancelIcon from "assets/cancel.svg";
+
+import "../MyDetails.scss";
 
 const EditEmployee = ({ next, prev }: any) => {
+  const modalPreview = useDisclosure();
+
   return (
     <div className="edit-emp form-card">
       <Input label="Name" type="text" />
@@ -14,8 +21,73 @@ const EditEmployee = ({ next, prev }: any) => {
 
       <div className="row">
         <Button label="Previous" variant="secondary--outline" onClick={prev} />
-        <Button label="Next" variant="primary" onClick={next} />
+        <Button label="Next" variant="primary" onClick={modalPreview.onOpen} />
       </div>
+      <Modal
+        open={modalPreview.isOpen}
+        onOk={modalPreview.onClose}
+        className="confirm-modal"
+        centered
+        style={{ top: 100 }}
+      >
+        <div className="modal-container">
+          <h3>Details Preview</h3>
+          <div className="row">
+            <p>Name</p>
+            <span>Solomon addy</span>
+          </div>
+          <hr className="view-emp-hr" />
+          <div className="row">
+            <p>RC number</p>
+            <span>123456789</span>
+          </div>
+          <hr className="view-emp-hr" />
+          <div className="row">
+            <p>Employer name</p>
+            <span>Capital FMC LTD</span>
+          </div>
+          <hr className="view-emp-hr" />
+          <div className="row">
+            <p>Salary amount</p>
+            <span>N500,000.00</span>
+          </div>
+          <hr className="view-emp-hr" />
+          <div className="row">
+            <p>Salary payment date</p>
+            <span>24/10/2022</span>
+          </div>
+          <hr className="view-emp-hr" />
+          <div className="row">
+            <p>Mobile number</p>
+            <span>08023654987</span>
+          </div>
+          <hr className="view-emp-hr" />
+          <div className="row">
+            <p>Email address </p>
+            <span>sol.abs@gmail.com</span>
+          </div>
+          <hr className="view-emp-hr" />
+          <div className="row">
+            <p>Salary account number </p>
+            <span>0123654987</span>
+          </div>
+          <hr className="view-emp-hr" />
+          <Button
+            label="Continue"
+            variant="primary"
+            onClick={() => {
+              modalPreview.onClose();
+              next();
+            }}
+          />
+          <img
+            className="close-icon"
+            onClick={modalPreview.onClose}
+            src={cancelIcon}
+            alt="cancelIcon"
+          />
+        </div>
+      </Modal>
     </div>
   );
 };
