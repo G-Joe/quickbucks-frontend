14326ca911ada4993aa88967296be2e9bfd1dcca
unogjoe <nketageorgejoe@gmail.com>
2022-11-04 10:38:45 +0100

    added icon to upload on account addeed arrows to footer on table in history

--- src/pages/Dashboard/Account/Open-account-components/upload-documents.tsx
+++ src/pages/Dashboard/Account/Open-account-components/upload-documents.tsx
@@ -1,26 +1,62 @@
-import Input from "components/shared/Input/Input";
 import Button from "components/shared/Button/Button";
-import "../account.scss"
+// import "../account.scss"
+import uploadIconImg from "assets/upload_icon.svg";
 
+import "../pages/MyDetails/MyDetails.scss";
 
 const Upload = ({next, prev}:any) => {
-    return (
-      <div className="edit-details">
-        <div className="form-wrapper">
-          <form className="form">
-            <Input label="Company ID" type="text" />
-            <Input label="Recent pay slip" type="text"/>
-            <Input label="Employment/contact letter" type="text"/>
-            <Input label="Government Issued ID" type="text"/>
-            <Input label="Proof of address" type="text"/>
-            <div className="account-buttons">
+  return (
+    <>
+      <div className="upload-emp form-card">
+        <div className="upload-group">
+          <label>Company ID</label>
+          <div className="upload-group-box">
+            <input className="upload-input" type="file" />
+            <img src={uploadIconImg} alt="uploadIconImg" />
+            <p>Upload</p>
+          </div>
+        </div>
+        <div className="upload-group">
+          <label>Recent pay slip</label>
+          <div className="upload-group-box">
+            <input className="upload-input" type="file" />
+            <img src={uploadIconImg} alt="uploadIconImg" />
+            <p>Upload</p>
+          </div>
+        </div>
+        <div className="upload-group">
+          <label>Employment/contact letter</label>
+          <div className="upload-group-box">
+            <input className="upload-input" type="file" />
+            <img src={uploadIconImg} alt="uploadIconImg" />
+            <p>Upload</p>
+          </div>
+        </div>
+        <div className="upload-group">
+          <label>Goverment Issued ID</label>
+          <div className="upload-group-box">
+            <input className="upload-input" type="file" />
+            <img src={uploadIconImg} alt="uploadIconImg" />
+            <p>Upload</p>
+          </div>
+        </div>
+        <div className="upload-group">
+          <label>Proof of address</label>
+          <div className="upload-group-box">
+            <input className="upload-input" type="file" />
+            <img src={uploadIconImg} alt="uploadIconImg" />
+            <p>Upload</p>
+          </div>
+        </div>
+        <div className="account-buttons">
               <Button label="Previous" variant="primary" onClick={prev}/>
               <Button label="Next" variant="primary" onClick={next}/>
             </div>
-          </form>
-        </div>
+        
       </div>
-    );
+
+    </>
+  );
   };
   
 
